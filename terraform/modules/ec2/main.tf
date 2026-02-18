9b82eac3a881123c9437613b85303f879e26d761
khaymanii <kennethakpo61@gmail.com>
2026-02-18 09:34:39 +0100

    fixed ami id

--- terraform/modules/ec2/main.tf
+++ terraform/modules/ec2/main.tf
@@ -1,20 +1,5 @@
-data "aws_ami" "ubuntu" {
-  most_recent = true
-  owners      = ["099720109477"]
-
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-noble-24.04-amd64-server-*"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-}
-
 resource "aws_instance" "this" {
-  ami                    = data.aws_ami.ubuntu.id
+  ami                    = ami-03446a3af42c5e74e
   instance_type          = var.instance_type
   key_name               = var.key_name
   vpc_security_group_ids = [var.security_group_id]
